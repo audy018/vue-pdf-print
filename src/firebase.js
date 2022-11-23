7b8c253a1e5e9ad2cd18ac6a97ea3444ab748ab9
audy018 <audy018@gmail.com>
2022-11-23 20:22:46 +0700

    1 file modified

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 let config = {
-  apiKey: "xxx",
-  authDomain: "bezkoder-firebase.firebaseapp.com",
-  databaseURL: "https://bezkoder-firebase.firebaseio.com",
-  projectId: "bezkoder-firebase",
-  storageBucket: "bezkoder-firebase.appspot.com",
-  messagingSenderId: "xxx",
-  appId: "xxx",
+  apiKey: "AIzaSyCMtW2AqoRWontXFTvLAcr6t3-7OlkYgzg",
+  authDomain: "vuejs-test-c04e5.firebaseapp.com",
+  projectId: "vuejs-test-c04e5",
+  storageBucket: "vuejs-test-c04e5.appspot.com",
+  messagingSenderId: "956100710301",
+  appId: "1:956100710301:web:a4de3db7ebd70149696974",
+  measurementId: "G-BVTPV365X7"
 };
 
 firebase.initializeApp(config);
